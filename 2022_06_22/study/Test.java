671925478b22f52c32ba82ff9775d0af1357092f
ylighgh <475254161@qq.com>
2022-06-23 07:58:15 +0800

    Update:mkdir

--- 2022_06_22/study/Test.java
+++ 2022_06_22/study/Test.java
@@ -2,7 +2,7 @@ package study;
 
 public class Test {
     public static void main(String[] args) {
-        FirstImpl first = new FirstImpl();
+        FirstInterface first = new FirstImpl();
         first.study();
         first.study("hy");
         first.study("yl","hy");
